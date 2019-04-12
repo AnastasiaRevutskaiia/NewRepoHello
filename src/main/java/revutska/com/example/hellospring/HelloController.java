1d80a1de3f0dfe1bc94fb478a2dcb3c01635a0b2
AnastasiaRevutskaiia <anastasiarp2000@gmail.com>
2019-04-12 17:06:09 +0300

    Delete @PropertySource

--- src/main/java/revutska/com/example/hellospring/HelloController.java
+++ src/main/java/revutska/com/example/hellospring/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@PropertySource("${classpath:application.yml}")
 public class HelloController {
 
     @Value("${hello-controller.prefix}")
